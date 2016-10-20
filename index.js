341e397d73b0d2d4fc5ae67c998e504bf2a79761
Jun <cygoh@umich.edu>
2016-10-19 17:13:53 -0700

    added broadcast when a user joins

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+	socket.broadcast.emit('chat message','user connected!')
 	socket.on('disconnect', function(){
     console.log('user disconnected');
+    io.emit('chat message','user disconnected!')
 	});
     socket.on('chat message', function(msg){
     console.log('message: ' + msg);
